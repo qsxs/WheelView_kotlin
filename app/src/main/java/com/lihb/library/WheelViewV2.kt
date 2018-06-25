a2f9905a6dbea77692ccbbb30caab03f38713307
Padaye <18218601735@139.com>
2018-06-25 14:55:40 +0800

    稍微修改一下别人的wheelView

--- app/src/main/java/com/lihb/library/WheelViewV2.kt
+++ app/src/main/java/com/lihb/library/WheelViewV2.kt
@@ -9,7 +9,9 @@ import android.support.annotation.StyleRes
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
+import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 
@@ -33,10 +35,31 @@ class WheelViewV2 : FrameLayout {
 
     private fun init(context: Context, attr: AttributeSet?) {
         val list = RecyclerView(context)
+        list.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+//                Log.i("TAG","onScrollStateChanged:$newState")
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                recyclerView?.scrollY
+                Log.i("TAG","onScrolled:${recyclerView?.scrollY},$dy")
+            }
+        })
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            list.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+//                Log.i("TAG","setOnScrollChangeListener:$scrollY,$oldScrollY")
+            }
+        }
         list.layoutManager = LinearLayoutManager(context)
         list.layoutParams = LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
         val adapter = WheelViewAdapter()
-        adapter.setNewData(listOf("HHH1","HHH2","HHH3","HHH4","HHH5","HHH6","HHH7","HHH8","HHH9","HHH1","HHH2","HHH3","HHH4","HHH5","HHH6","HHH7","HHH8","HHH9"))
+        var data:ArrayList<String> = arrayListOf()
+        for (i in 1..100){
+            data.add("HHH$i")
+        }
+        adapter.setNewData(data)
         list.adapter = adapter
         addView(list)
     }
@@ -47,6 +70,31 @@ class WheelViewV2 : FrameLayout {
             startScrollerTask()
         }
         return super.onTouchEvent(ev)
+    }
+
+    override fun onScrollChanged(l: Int, t: Int, oldl: Int, oldt: Int) {
+        super.onScrollChanged(l, t, oldl, oldt)
+        Log.i("TAG","onScrollChanged：$l,$t,$oldl,$oldt")
+    }
+
+    override fun onStopNestedScroll(child: View?) {
+        super.onStopNestedScroll(child)
+        Log.i("TAG", "onStopNestedScroll")
+    }
+
+    override fun onNestedScrollAccepted(child: View?, target: View?, axes: Int) {
+        super.onNestedScrollAccepted(child, target, axes)
+        Log.i("TAG", "onNestedScrollAccepted $axes")
+    }
+
+    override fun onOverScrolled(scrollX: Int, scrollY: Int, clampedX: Boolean, clampedY: Boolean) {
+        super.onOverScrolled(scrollX, scrollY, clampedX, clampedY)
+        Log.i("TAG", "onOverScrolled $scrollX,$scrollY,$clampedX,$clampedY")
+    }
+
+    override fun onNestedPreScroll(target: View?, dx: Int, dy: Int, consumed: IntArray?) {
+        super.onNestedPreScroll(target, dx, dy, consumed)
+        Log.i("TAG", "onNestedPreScroll $dx,$dy,$consumed")
     }
 
     private fun startScrollerTask() {
--- app/src/main/java/com/lihb/wheelview/MainActivity.kt
+++ app/src/main/java/com/lihb/wheelview/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.lihb.wheelview
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.lihb.library.WheelView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val data = arrayListOf<String>()
+        for (i in 0..50){
+            data.add("HAHAHHAHAHHAHHHAHAH $i")
+        }
+        wheel_view.setItems(data)
+        wheel_view.selectedIndex = 0
+        wheel_view.setCycleDisable(false)
+        wheel_view.setVisibleItemCount(5)
+        wheel_view.setTextSize(15F)
+//        wheel_view.setDividerColor(Color.BLUE)
+        val dividerConfig = WheelView.DividerConfig()
+        dividerConfig.setColor(Color.BLUE)
+        dividerConfig.setRatio(0F)
+        wheel_view.setDividerConfig(dividerConfig)
+        wheel_view.setLabel("label",false)
+        wheel_view.setTextColor(Color.GREEN)
+        wheel_view.setTextSizeAutoFit(false)
+//        wheel_view.setUseWeight(false)
+//        wheel_view.setGravity(Gravity.RIGHT)
+        wheel_view.setOnItemSelectListener { index ->
+            Toast.makeText(MainActivity@this,"选中$index",Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
