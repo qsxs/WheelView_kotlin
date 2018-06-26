1e933277e38343892de7aa1e616632a22baf8608
Padaye <18218601735@139.com>
2018-06-26 14:08:24 +0800

    wheelView2D

--- app/src/main/java/com/lihb/wheelview/MainActivity.kt
+++ app/src/main/java/com/lihb/wheelview/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import com.lihb.library.WheelView
+import com.lihb.library2d.adapter.NumericWheelAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,27 +14,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val data = arrayListOf<String>()
-        for (i in 0..50){
+        for (i in 0..50) {
             data.add("HAHAHHAHAHHAHHHAHAH $i")
         }
         wheel_view.setItems(data)
         wheel_view.selectedIndex = 0
         wheel_view.setCycleDisable(false)
-        wheel_view.setVisibleItemCount(5)
+        wheel_view.setVisibleItemCount(7)
         wheel_view.setTextSize(15F)
 //        wheel_view.setDividerColor(Color.BLUE)
         val dividerConfig = WheelView.DividerConfig()
         dividerConfig.setColor(Color.BLUE)
         dividerConfig.setRatio(0F)
         wheel_view.setDividerConfig(dividerConfig)
-        wheel_view.setLabel("label",false)
+        wheel_view.setLabel("label", false)
         wheel_view.setTextColor(Color.GREEN)
         wheel_view.setTextSizeAutoFit(false)
 //        wheel_view.setUseWeight(false)
 //        wheel_view.setGravity(Gravity.RIGHT)
         wheel_view.setOnItemSelectListener { index ->
-            Toast.makeText(MainActivity@this,"选中$index",Toast.LENGTH_SHORT).show()
+            Toast.makeText(MainActivity@ this, "选中$index", Toast.LENGTH_SHORT).show()
         }
 
+        val adapter = NumericWheelAdapter(this, 0, 50)
+        wheel_view2.viewAdapter = adapter
+        wheel_view2.visibleItems = 10
+        wheel_view2.isCyclic = true
+        wheel_view2.currentItem = 20
+        adapter.setLabel("")
+        adapter.textSize = 15
+        adapter.textColor = Color.BLACK
+        wheel_view2.addClickingListener { wheel, itemIndex ->
+            Toast.makeText(MainActivity@ this, "选中$itemIndex", Toast.LENGTH_SHORT).show()
+        }
+        wheel_view2.addChangingListener { wheel, oldValue, newValue ->
+            Toast.makeText(MainActivity@ this, "Changing $newValue", Toast.LENGTH_SHORT).show()
+        }
     }
 }
